4eddbb68d74da2a97fe6185de9debaaafa05ed54
adityabalhara <aditya.balhara@gmail.com>
2018-02-16 15:28:49 +0000

    Updated function

--- stack_access.sh
+++ stack_access.sh
@@ -28,7 +28,8 @@ StackParams='{
 }'
 
 function CreateRFC() {
-  RfcInput=$1
+echo "inside the createRFC function" 
+ RfcInput=$1
   RfcParams=$2
   _RfcOutput=$3
 
@@ -38,24 +39,38 @@ function CreateRFC() {
   RfcInput="${RfcInput/START_TIME/$start_time}"
   RfcInput="${RfcInput/END_TIME/$end_time}"
 
+
+
   # Create RFC
-  Output=`/usr/bin/aws amscm create-rfc --cli-input-json "$RfcInput" --execution-parameters "$RfcParams"`
+  Output=`aws amscm create-rfc --cli-input-json "$RfcInput" --execution-parameters "$RfcParams" --region us-east-1`
   if [ -z "$Output" ]; then
     echo 'RFC failed to create'
     exit 1
   fi
-  RfcId=`echo "$Output" | sed 's/"//g'`
+  echo 'create-rfc Output ' $Output
+  RfcId=$( echo $Output | jq '.RfcId' )
+  RfcId=`echo "$RfcId" | sed 's/"//g'`
   echo 'Created RFC: ' $RfcId
 
   #Submit RFC
-  Submit=`/usr/bin/aws amscm submit-rfc --rfc-id $RfcId`
+  Submit=`aws amscm submit-rfc --rfc-id $RfcId --region us-east-1`
+  echo 'submit-rfc Output ' $Submit
 
   #Wait until RFC is completed
   RfcStatus=''
+  count=1
   while [ "$RfcStatus" != "Success" ] && [ "$RfcStatus" != "Failure" ] && [ "$RfcStatus" != "Canceled" ] && [ "$RfcStatus" != "Rejected" ]; do
     sleep 10
-    Rfc=`aws amscm get-rfc --rfc-id $RfcId --output json`
+    Rfc=`aws amscm get-rfc --rfc-id $RfcId --output json --region us-east-1`
+	#echo 'rfc: ' $Rfc
     RfcStatus=`echo $Rfc | jq '.Rfc.Status.Id' | sed 's/"//g'`
+    echo 'RfcStatus: ' $RfcStatus
+	((count++))
+  if [ $count -eq 360 ]
+    then
+       printf "time for a break,count has a value of $count\n"
+       break
+  fi     
   done
 
   # Set _RfcOutput variable and return
@@ -63,10 +78,7 @@ function CreateRFC() {
     echo 'RFC failed to execute'
     exit 1
   fi
-  eval $_RfcOutput="'$Rfc'"
-  return 0
 }
-
 
 # 1 - Create Access Stack
 StackRfcOutput=""
